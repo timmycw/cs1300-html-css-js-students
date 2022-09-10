5810ac29aabe66fb080463545b0437428e469686
Timmy Wang <tcwang@macbook-pro-10.devices.brown.edu>
2022-09-10 17:20:37 -0400

    studio 3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -40,15 +40,18 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
-
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const filtered = fruitsArray.filter(x => {return x.nutritions.sugar > 10});
+  const filteredFruits = filtered.map(x => {return x.name});
 
   // TODO: Create a new HTML element to display your data 
+  
+  const sugarFruits = document.createElement('div');
+  sugarFruits.innerHTML = filteredFruits;
 
   // TODO: Append your new element to the page
-
+  gallery.append(sugarFruits);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
